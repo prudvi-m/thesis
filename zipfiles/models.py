3c2a3c5a8f3cd48abfc58eb70157acb2f7b8a180
prudvi-m <prudhvi.manukonda4@google.com>
2023-07-04 14:43:02 -0500

    assignment_number

--- zipfiles/models.py
+++ zipfiles/models.py
@@ -6,30 +6,6 @@ from django.utils import timezone
 import os
 
 # Create your models here.
-class File_Results(models.Model):
-  id_number = models.PositiveIntegerField(null=True)
-  f_name = models.CharField(max_length=255,null=True)
-  f_size = models.CharField(max_length=50,null=True)
-  myfiles = models.FileField(upload_to="")
-  user_name = models.ForeignKey('UserNamesList', on_delete=models.CASCADE, null=True)
-  db_name = models.CharField(max_length=50,null=True)
-  is_build_succeeded = models.CharField(max_length=50,null=True)
-  dotnet_version = models.CharField(max_length=20,null=True)
-  
-  def __str__(self):
-    return f'files: {self.user_name} {self.db_name}'
-  
-@receiver(pre_delete, sender=File_Results)
-def delete_file_on_delete(sender, instance, **kwargs):
-    # Get the file path
-    file_path = instance.myfiles.path
-    
-    # Check if the file exists and delete it
-    if os.path.exists(file_path):
-        os.remove(file_path)
-
-pre_delete.connect(delete_file_on_delete, sender=File_Results)
-
 class UserNamesList(models.Model):
     user_name = models.CharField(
         max_length=255,
@@ -48,3 +24,30 @@ class UserNamesList(models.Model):
         super().save(*args, **kwargs)
     class Meta:
         verbose_name_plural = 'UserNamesList'
+
+
+class File_Results(models.Model):
+  id_number = models.PositiveIntegerField(null=True)
+  f_name = models.CharField(max_length=255,null=True)
+  f_size = models.CharField(max_length=50,null=True)
+  myfiles = models.FileField(upload_to="")
+  user_name = models.ForeignKey('UserNamesList', on_delete=models.CASCADE, null=True)
+  db_name = models.CharField(max_length=50,null=True)
+  is_build_succeeded = models.CharField(max_length=50,null=True)
+  dotnet_version = models.CharField(max_length=20,null=True)
+  assignment_number = models.IntegerField(null=True)
+
+  def __str__(self):
+    return f'files: {self.user_name} {self.db_name}'
+  
+@receiver(pre_delete, sender=File_Results)
+def delete_file_on_delete(sender, instance, **kwargs):
+    # Get the file path
+    file_path = instance.myfiles.path
+    
+    # Check if the file exists and delete it
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+pre_delete.connect(delete_file_on_delete, sender=File_Results)
+
