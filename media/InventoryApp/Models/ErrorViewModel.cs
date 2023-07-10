280bf1e0914f6d86accefbb06563b912fe2fbfa0
prudvi-m <prudhvi.manukonda4@google.com>
2023-07-10 07:38:15 -0500

    ok

---
+++ media/InventoryApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InventoryMid.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ media/RideApp/Controllers/RidesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RideApp.Models;
+
+namespace RideApp.Controllers
+{
+    public class RidesController : Controller
+    {
+        private readonly RideReservationDbContext _context;
+
+        public RidesController(RideReservationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rides
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Rides.ToListAsync());
+        }
+
+        // GET: Rides/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ride = await _context.Rides
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ride == null)
+            {
+                return NotFound();
+            }
+
+            return View(ride);
+        }
+
+        // GET: Rides/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rides/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,StartLocation,EndLocation,RideDate,CarModel,PassengerName,DriverName,Status,ReservationDate")] Ride ride)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ride);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ride);
+        }
+
+        // GET: Rides/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ride = await _context.Rides.FindAsync(id);
+            if (ride == null)
+            {
+                return NotFound();
+            }
+            return View(ride);
+        }
+
+        // POST: Rides/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,StartLocation,EndLocation,RideDate,CarModel,PassengerName,DriverName,Status,ReservationDate")] Ride ride)
+        {
+            if (id != ride.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ride);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RideExists(ride.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ride);
+        }
+
+        // GET: Rides/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ride = await _context.Rides
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ride == null)
+            {
+                return NotFound();
+            }
+
+            return View(ride);
+        }
+
+        // POST: Rides/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ride = await _context.Rides.FindAsync(id);
+            _context.Rides.Remove(ride);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RideExists(int id)
+        {
+            return _context.Rides.Any(e => e.Id == id);
+        }
+    }
+}
